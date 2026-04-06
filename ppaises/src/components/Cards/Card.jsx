0bb868bad39de546818f0f7e3d448d7ff310a429
upigava10-ctrl <upi.gava10@gmail.com>
2026-04-05 20:07:35 -0600

    feat: Completed Cards

--- ppaises/src/components/Cards/Card.jsx
+++ ppaises/src/components/Cards/Card.jsx
@@ -2,9 +2,41 @@ import React from 'react';
 import './Card.css'
 
 const Card = ({country, method}) => {
+    // console.log(Object.values(country.currencies)[0].name)
+    const getCurrencyName = (currencies) => {
+        if (Object.keys(currencies).length >0) {
+            return Object.values(currencies)[0].name;
+        }else{
+            return "No definida"
+        }
+    };
+    const getCurrencySymbol = (currencies) => {
+        if (Object.keys(currencies).length >0) {
+            console.log(Object.values(country.currencies)[0].symbol)
+            return Object.values(currencies)[0].symbol;
+        }else{ 
+            return "No definida"
+        }
+    };
+    
     return (
         <button onClick={method} className='btn'>
-            {country.name.common}
+            <h3>Nombre Común:</h3>
+            <p>{country.name.common}</p>
+            
+            <h3>Nombre Oficial:</h3>
+            <p>{country.name.official}</p>
+            
+            <h3>Capital:</h3>
+            <p>{country.capital}</p>
+
+            <h3>Nombre de moneda:</h3>
+            <p>{getCurrencyName(country.currencies)}</p>
+
+            <h3>Símbolo:</h3>
+            <p>{getCurrencySymbol(country.currencies)}</p>
+
+
         </button>
     );
 };
